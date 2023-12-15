c8f8b4b0d9b98e65aa3a471324d3797e8b0f3ea1
Matt Wuring <100162714+wuringMatt@users.noreply.github.com>
2023-12-15 22:40:07 +0100

    Title + introduction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+</script>
+
+<section> 
+    <h1>
+        <span>My name is Matt</span><br>
+        <span>and I'm a Software Developer.</span>
+    </h1>
+    <div class="warning">
+        <div class="warningText">
+            <span class="material-symbols-outlined">warning</span>
+            <p>Warning:</p>
+        </div>
+        <p>This website is still under construction. Please be aware of this whilst browsing.</p>
+    </div>
+</section>
+
+<style scoped>
+    section{
+        width: 100%;
+        padding: 100px 25% 0 25%;
+        display: grid;
+        grid-template-columns: 1fr;
+        gap: 25px;
+    }
+
+    h1{
+        font-size: 3.8rem;
+        font-weight: 600;
+    }
+
+    .warning{
+        border: 1px solid rgba(253, 137, 42, 0.747);
+        border-radius: 10px;
+        padding: 10px;
+        display: grid;
+        grid-template-columns: 100px 1fr;
+        gap: 20px;
+    }
+
+    .warning .warningText{
+        display: flex;
+    }
+
+    .warning p{
+        color: rgba(253, 137, 42, 0.747);
+        display: flex;
+        align-items: center;
+    }
+
+    .warning span{
+        color:rgba(253, 137, 42, 0.747)
+    }
+</style>
